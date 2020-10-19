84083dd42d110523640fad11b1553f155b41a4be
joel <joel_verhagen_1997@hotmail.com>
2020-10-19 20:48:01 +0200

    viewmodel

--- app/src/main/java/com/example/madlevel5example/Models/ReminderViewModel.kt
+++ app/src/main/java/com/example/madlevel5example/Models/ReminderViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.madlevel5example.Models
 
-class ReminderViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.madlevel2example.Reminder
+import com.example.madlevel5example.Repository.ReminderRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ReminderViewModel(application: Application)  : AndroidViewModel(application) {
+
+    private val ioScope = CoroutineScope(Dispatchers.IO)
+    private val reminderRepository = ReminderRepository(application.applicationContext)
+    val reminders: LiveData<List<Reminder>> = reminderRepository.getAllReminders()
+
+
+    fun insertReminder(reminder: Reminder) {
+        ioScope.launch {
+            reminderRepository.insertReminder(reminder)
+        }
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        ioScope.launch {
+            reminderRepository.deleteReminder(reminder)
+        }
+    }
+
+
+
+
 }
